1e7ee27327320aefeae33fcfde60d4170f454419
Wilson Bonato <wilsonbonatojr@gmail.com>
2025-05-17 12:52:26 -0300

    Add backend initial folders

---
+++ backend/src/main/java/bonatowil/github/io/smartsolar/SmartSolarApplication.java
@@ -0,0 +1,13 @@
+package bonatowil.github.io.smartsolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartSolarApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/bonatowil/github/io/smartsolar/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bonatowil.github.io.smartsolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
